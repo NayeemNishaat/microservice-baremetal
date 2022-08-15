9813e1299738c08b346da9623f60e71c4c05359d
nayeemnishaat <rxnayeemimi@gmail.com>
2022-08-15 21:44:26 +0600

    updated client

--- client/src/CommentList.jsx
+++ client/src/CommentList.jsx
@@ -1,10 +1,27 @@
 const CommentList = ({ comments }) => {
   // Important: Now to making any extra requests to get the comments.
   return (
-    <ul>
-      {comments.map((comment) => (
-        <li key={comment.id}>{comment.content}</li>
-      ))}
+    <ul className="list-disc list-inside">
+      {comments.map((comment) => {
+        let content;
+
+        switch (comment.status) {
+          case "approved":
+            content = comment.content;
+            break;
+          case "pending":
+            content = "Awaiting approval!";
+            break;
+          case "rejected":
+            content = "Comment has been rejected!";
+            break;
+          default:
+            content = "Awaiting approval!";
+            break;
+        }
+
+        return <li key={comment.id}>{content}</li>;
+      })}
     </ul>
   );
 };
